f6accda4bdb765666a8325fae9fe3157cc004776
Kayla Fang <kfang00@stuy.edu>
2019-04-14 19:12:40 -0400

    added code for apply

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -58,7 +58,19 @@ public class ExpressionTree{
   /*use the correct operator on both a and b, and return that value*/
   private double apply(char op, double a, double b){
     /*you are to write this method*/
-    return a op b;
+    if (op == '+') {
+      return a + b;
+    }
+    else if (op == '/') {
+      return a / b;
+    }
+    else if (op == '-') {
+      return a - b;
+    }
+    else if (op == '*') {
+      return a * b;
+    }
+    return a % b;
 
     }
 
